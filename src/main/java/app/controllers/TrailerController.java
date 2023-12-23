8abdf1350accd697a8a578603cd3c2fb24ec536f
Luca <47753569+lucacodorean@users.noreply.github.com>
2023-12-23 15:42:38 +0200

    chore: Eliminated useless imports.

--- src/main/java/app/controllers/TrailerController.java
+++ src/main/java/app/controllers/TrailerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import app.single_point_access.ServiceSinglePointAccess;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import app.dto.TrailerUpdateRequest;
