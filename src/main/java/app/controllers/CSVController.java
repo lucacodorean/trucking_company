1da5e0471d65732c22e724263450fc23bdeba136
Luca <47753569+lucacodorean@users.noreply.github.com>
2023-12-25 00:09:42 +0200

    feat: Finalized first version of the CSV import & export system.

--- src/main/java/app/controllers/CSVController.java
+++ src/main/java/app/controllers/CSVController.java
@@ -1,23 +1,23 @@
 package app.controllers;
 
+import org.assertj.core.error.ShouldBeIn;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.lang.reflect.Field;
-import java.util.List;
-
-import app.dto.CSVRequest;
-import app.models.*;
-import app.services.*;
 import app.single_point_access.ServiceSinglePointAccess;
+import net.bytebuddy.utility.RandomString;
+import java.text.SimpleDateFormat;
+import java.lang.reflect.Field;
+import app.dto.CSVRequest;
 import app.util.FileUtil;
+import app.services.*;
+import app.models.*;
+import java.util.*;
+import java.io.*;
 
 @RestController
 @RequestMapping("/api/v1/csv/")
@@ -170,7 +170,113 @@ public class CSVController {
 
         } catch(Exception ex) { 
             System.out.println(ex.toString()); 
-             return ResponseEntity.status(HttpStatus.OK).body(false);
+            return ResponseEntity.status(HttpStatus.OK).body(false);
+        }
+    }
+
+    @PostMapping("/import")
+    public ResponseEntity<Boolean> importModelfromCSV(@RequestBody CSVRequest request) {
+        try {
+
+            getFields(request.getClassName());
+
+            File file = FileUtil.getAndCreateFileFromResourcesDirectory(request.getFileName());
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+            String line = "";
+            String[] attributes = {};
+            String[] headers    = {};
+            HashMap<String, String> dictionary = new HashMap<>();
+    
+
+            boolean header = true;
+            while((line = bufferedReader.readLine()) != null) {
+                attributes = line.split(",");
+                
+                if(header) {
+                    headers = Arrays.copyOf(attributes, attributes.length);
+                    header = false;
+                    continue;
+                }
+                
+                for(int i = 0; i<attributes.length; i++) dictionary.put(headers[i], attributes[i]);        
+                System.out.println(dictionary.entrySet());
+                
+
+                if(objectClass instanceof User) {
+                    ((User)objectClass).setName(dictionary.get("name"));
+                    ((User)objectClass).setEmail(dictionary.get("email"));
+                    ((User)objectClass).setPassword(dictionary.get("password"));
+                    ((User)objectClass).setPhoneNumber(dictionary.get("phoneNumber"));
+                    ((User)objectClass).setRole(roleService.findByKey(dictionary.get("role")));
+                    ((User)objectClass).setAddress(addressService.findByKey(dictionary.get("address")));
+                    ((User)objectClass).setEmploymentDate(new SimpleDateFormat("dd-mm-yyyy").parse(dictionary.get("employmentDate")));
+                    ((User)objectClass).setModelKey(((User)objectClass).getClass().getSimpleName().toLowerCase().substring(0, 3) + "_" + RandomString.make(25));
+                    userService.save((User)objectClass);
+                }
+
+                if(objectClass instanceof Role) {
+                    ((Role)objectClass).setModelKey(((Role)objectClass).getClass().getSimpleName().toLowerCase().substring(0, 3) + "_" + RandomString.make(25));
+                    ((Role)objectClass).setPosition(dictionary.get("position"));
+                    ((Role)objectClass).setSalary(dictionary.get("salary"));
+                    roleService.save((Role)objectClass);
+                }
+
+                if(objectClass instanceof Address) {
+                    ((Address)objectClass).setCity(dictionary.get("city"));
+                    ((Address)objectClass).setRegion(dictionary.get("region"));
+                    ((Address)objectClass).setStreet(dictionary.get("street"));
+                    ((Address)objectClass).setZipCode(dictionary.get("zipcode"));
+                    ((Address)objectClass).setModelKey(((Address)objectClass).getClass().getSimpleName().toLowerCase().substring(0, 3) + "_" + RandomString.make(25));
+                    addressService.save((Address)objectClass);
+                }
+
+                if(objectClass instanceof ActivityLog) {
+                    ((ActivityLog)objectClass).setDateOfDelivery(new SimpleDateFormat("dd-mm-yyyy").parse(dictionary.get("dateOfDelivery")));
+                    ((ActivityLog)objectClass).setModelKey(((ActivityLog)objectClass).getClass().getSimpleName().toLowerCase().substring(0, 3) + "_" + RandomString.make(25));
+                    ((ActivityLog)objectClass).setShipment(shipmentService.findByKey(dictionary.get("shipment")));
+                    ((ActivityLog)objectClass).setTrailer(trailerService.findByKey(dictionary.get("trailer")));
+                    ((ActivityLog)objectClass).setTruck(truckService.findByKey(dictionary.get("truck")));
+                    ((ActivityLog)objectClass).setUser(userService.findByKey(dictionary.get("user")));
+                    activityService.save((ActivityLog)objectClass);
+                }
+
+                if(objectClass instanceof Trailer) {
+                    ((Trailer)objectClass).setMaximumCapacity(Integer.parseInt(dictionary.get("maximumCapacity")));
+                    ((Trailer)objectClass).setPlateNumber(dictionary.get("plateNumber"));
+                    ((Trailer)objectClass).setType(dictionary.get("type"));
+                    ((Trailer)objectClass).setModelKey(((Trailer)objectClass).getClass().getSimpleName().toLowerCase().substring(0, 3) + "_" + RandomString.make(25));
+                    trailerService.save((Trailer)objectClass);
+                }
+
+                if(objectClass instanceof Truck) {
+                    ((Truck)objectClass).setBrand(dictionary.get("brand"));
+                    ((Truck)objectClass).setVin(dictionary.get("vin"));
+                    ((Truck)objectClass).setNumberPlate(dictionary.get("numberPlate"));
+                    ((Truck)objectClass).setAquiredAt(new SimpleDateFormat("dd-mm-yyyy").parse(dictionary.get("aquiredAt")));
+                    ((Truck)objectClass).setLastPVI(new SimpleDateFormat("dd-mm-yyyy").parse(dictionary.get("lastPVI")));
+                    ((Truck)objectClass).setHubAddress(addressService.findByKey(dictionary.get("hubAddress")));
+                    ((Truck)objectClass).setModelKey(((Truck)objectClass).getClass().getSimpleName().toLowerCase().substring(0, 3) + "_" + RandomString.make(25));
+                    truckService.save((Truck)objectClass);
+                }
+            
+                if(objectClass instanceof Shipment) {
+                    ((Shipment)objectClass).setDescription(dictionary.get("description"));
+                    ((Shipment)objectClass).setPrice(Integer.parseInt(dictionary.get("price")));
+                    ((Shipment)objectClass).setCargoHeight(Float.parseFloat(dictionary.get("cargoHeight")));
+                    ((Shipment)objectClass).setDistanceInKM(Float.parseFloat(dictionary.get("distanceInKm")));
+                    ((Shipment)objectClass).setDestination(addressService.findByKey(dictionary.get("destination")));
+                    ((Shipment)objectClass).setPickUpPoint(addressService.findByKey(dictionary.get("pickUpPoint")));
+                    ((Shipment)objectClass).setModelKey(((Shipment)objectClass).getClass().getSimpleName().toLowerCase().substring(0, 3) + "_" + RandomString.make(25));
+                    shipmentService.save((Shipment)objectClass);
+                }
+            }
+            
+            bufferedReader.close();
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+        } catch(Exception ex) {
+            System.out.println(ex.toString());
+            return ResponseEntity.status(HttpStatus.OK).body(false);
         }
     }
 }
